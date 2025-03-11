92c95c06a97b82912c99d481b66d7d133761fd75
OUROUA M. Mounib <ourouamounib@gmail.com>
2025-03-11 15:06:57 +0100

    App initialization

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UsersModule } from "./users/users.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "./users/user.entity";
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "postgres",
+      password: "mnbmoon345",
+      database: "esgi-prj-1",
+      entities: [User],
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
