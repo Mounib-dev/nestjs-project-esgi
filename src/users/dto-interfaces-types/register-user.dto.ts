8c98d50bb50f6a9e4a637d0c47b4486aa36a9b58
OUROUA M. Mounib <ourouamounib@gmail.com>
2025-03-11 15:08:11 +0100

    Register User DTO

---
+++ src/users/dto-interfaces-types/register-user.dto.ts
@@ -0,0 +1,19 @@
+import { IsEmail, IsNotEmpty, MinLength } from "class-validator";
+import { UserRole } from "./user-role.type";
+
+export class RegisterUserDto {
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+
+  @IsNotEmpty()
+  firstname: string;
+
+  @IsNotEmpty()
+  lastname: string;
+
+  role: UserRole;
+}
