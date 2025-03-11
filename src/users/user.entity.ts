bc3454b267b5d26b29df8ea945dddfe548064b80
OUROUA M. Mounib <ourouamounib@gmail.com>
2025-03-11 15:07:52 +0100

    User Entity

---
+++ src/users/user.entity.ts
@@ -0,0 +1,31 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+} from "typeorm";
+import { UserRole } from "./dto-interfaces-types/user-role.type";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  firstname: string;
+
+  @Column()
+  lastname: string;
+
+  @Column({ type: "enum", enum: UserRole })
+  role: UserRole;
+
+  @CreateDateColumn({ type: "timestamp" })
+  createdAt: Date;
+}
