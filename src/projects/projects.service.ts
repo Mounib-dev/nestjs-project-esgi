78b19922327f299b866b8d6c658e4216befce5a7
OUROUA M. Mounib <ourouamounib@gmail.com>
2025-04-11 16:12:50 +0200

    Service: create project with assigning its user

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,19 +1,54 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { Project } from "./project.entity";
 import { CreateProjectDto } from "./dto-interfaces-types/create-project.dto";
+import { User } from "src/users/user.entity";
+
+export type ProjectWithoutUser = Pick<
+  Project,
+  "id" | "title" | "description" | "budget" | "category"
+>;
 
 @Injectable()
 export class ProjectsService {
   constructor(
     @InjectRepository(Project)
     private projectsRepository: Repository<Project>,
+    @InjectRepository(User) private userRepositry: Repository<User>,
   ) {}
 
-  async create(createProjectDto: CreateProjectDto): Promise<Project> {
-    const project = this.projectsRepository.create(createProjectDto);
-    return this.projectsRepository.save(project);
+  async create(
+    createProjectDto: CreateProjectDto,
+    user: User,
+  ): Promise<ProjectWithoutUser> {
+    const currentUser = await this.userRepositry.findOne({ where: user });
+    if (!currentUser) {
+      throw new NotFoundException("Aucun utilisateur trouvé");
+    }
+
+    const { title, description, budget, category } = createProjectDto;
+    const project = this.projectsRepository.create({
+      title,
+      description,
+      budget,
+      category,
+      user: currentUser,
+    });
+
+    try {
+      await this.projectsRepository.save(project);
+    } catch (err) {
+      console.error(err);
+    }
+    const createdProject = {
+      id: project.id,
+      title,
+      description,
+      budget,
+      category,
+    };
+    return createdProject;
   }
 
   async findAll(): Promise<Project[]> {
