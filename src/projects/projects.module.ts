1b7dfd89633d8ee6d619abfd6ebd80d29860ea55
OUROUA M. Mounib <ourouamounib@gmail.com>
2025-04-11 16:13:16 +0200

    Adding User Module to be injectable in the Project Module

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -4,9 +4,10 @@ import { ProjectsService } from "./projects.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Project } from "./project.entity";
 import { AuthModule } from "src/auth/auth.module";
+import { UsersModule } from "src/users/users.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Project]), AuthModule],
+  imports: [TypeOrmModule.forFeature([Project]), AuthModule, UsersModule],
   controllers: [ProjectsController],
   providers: [ProjectsService],
 })
