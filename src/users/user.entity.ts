4513bf1e9aeb0ff92ff2c7075dc50dfb584072f6
OUROUA M. Mounib <ourouamounib@gmail.com>
2025-04-11 16:11:28 +0200

    Adding relation between User and Project

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -3,8 +3,10 @@ import {
   Column,
   PrimaryGeneratedColumn,
   CreateDateColumn,
+  OneToMany,
 } from "typeorm";
 import { UserRole } from "./dto-interfaces-types/user-role.type";
+import { Project } from "src/projects/project.entity";
 
 @Entity()
 export class User {
@@ -25,6 +27,9 @@ export class User {
 
   @Column({ type: "enum", enum: UserRole, default: UserRole.ENTREPRENEUR })
   role: UserRole;
+
+  @OneToMany((_type) => Project, (project) => project.user, { eager: true })
+  projects: Project[];
 
   @CreateDateColumn({ type: "timestamp" })
   createdAt: Date;
